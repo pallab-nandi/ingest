dc82365291726395f33b6cd5a3297f8f660ed78e
pallab-nandi <pallabnandi@outlook.in>
2025-02-08 22:14:00 +0530

    added: settings and urls

--- ingest/settings.py
+++ ingest/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+from dotenv import load_dotenv
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ingest_be',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +125,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Load environment variables
+
+load_dotenv()
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
---
+++ ingest_be/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+app_name = 'ingest_be'
+
+urlpatterns = [
+    path('', '<h1>Title</h1>', name='index'),
+]
